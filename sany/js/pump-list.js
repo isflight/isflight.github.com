33d347e793c1b0383b8980387190bd89d0fb746e
isfly <2192068349@qq.com>
2016-05-13 08:54:29 +0800

    change sany

--- sany/js/pump-list.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-$(document).ready(function(){
-	$("#allbtn").click(function(){
-	  $("#product-name").animate({
-		height:'toggle'
-	  });
-	  $(this).toggleClass('on');
-	});
-});
